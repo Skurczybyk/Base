30edded78afe2789c8a405f533432a4a6ed1fb25
Marek Podolak <podolakmarek9119@gmail.com>
2023-12-11 12:47:30 +0100

    update config 4

--- Bootstrap/src/main/java/com/Base/Bootstrap/Main.java
+++ Bootstrap/src/main/java/com/Base/Bootstrap/Main.java
@@ -2,9 +2,16 @@ package com.Base.Bootstrap;
 
 import com.Base.Update.Updater;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Updater.updatePrint();
+/*        try {
+            Updater.makeUpdate();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
     }
 }
--- Update/src/main/java/com/Base/Update/Updater.java
+++ Update/src/main/java/com/Base/Update/Updater.java
@@ -1,11 +1,19 @@
 package com.Base.Update;
 
+import org.update4j.Archive;
 import org.update4j.Configuration;
 import org.update4j.FileMetadata;
+import org.update4j.UpdateOptions;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Updater {
@@ -26,5 +34,14 @@ public class Updater {
             throw new RuntimeException(e);
         }
 
+    }
+    public static void makeUpdate() throws IOException {
+        URL configURL = new URL("https://raw.githubusercontent.com/Skurczybyk/Base/master/config.xml");
+        Reader in = new InputStreamReader(configURL.openStream(), StandardCharsets.UTF_8);
+        Configuration config = null;
+        config = Configuration.read(in);
+        Path zip = Paths.get("up.zip");
+        config.update(UpdateOptions.archive(zip));
+        Archive.read(zip).install();
     }
 }
